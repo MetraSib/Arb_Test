81474384f71c7e36254e2c38589c8f7fe82f01a4
MetraSib <70145795+MetraSib@users.noreply.github.com>
2020-12-26 17:09:55 +0300

    Добавлен бесплатный ассет с графикой. Создание анимации. Настроен задний фон.

---
+++ Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    private Vector3 startVector = Vector3.zero;
+
+    private float finishBoundary = -19.2f;
+
+    [SerializeField] private float speed;
+
+    void Update()
+    {
+        if (transform.position.x <= finishBoundary)
+        {
+            transform.position = startVector;
+        }
+
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}
