51432d9991dfedc49b6f01c1133765d1b49cb880
MetraSib <70145795+MetraSib@users.noreply.github.com>
2020-12-29 19:00:44 +0300

    Настроены UI панели. Добавлен REST CLIENT.

---
+++ Assets/Scripts/GamePanelScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePanelScript : MonoBehaviour
+{
+    [SerializeField] private GameObject gamePausePanel;
+
+    public void PauseMenu() 
+    {
+        Time.timeScale = 0;
+        gamePausePanel.SetActive(true);
+    }
+
+    public void ContinueGame() 
+    {
+        Time.timeScale = 1;
+        gamePausePanel.SetActive(false);
+    }
+
+    public void BackMainMenu() 
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame() 
+    {
+        Application.Quit();
+    }
+
+}
