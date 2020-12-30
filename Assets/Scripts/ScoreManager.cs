c490e2a67d53037890fbd96b3714b7b592a5e289
MetraSib <70145795+MetraSib@users.noreply.github.com>
2020-12-30 03:25:32 +0300

    Добавлены плагины для работы SQLite.

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private void Start()
+    {
+        // Получаем отсортированную таблицу лидеров
+        DataTable scoreboard = MyDataBase.GetTable("SELECT * FROM Scores ORDER BY score DESC;");
+        // Получаем id лучшего игрока
+        int idBestPlayer = int.Parse(scoreboard.Rows[0][1].ToString());
+        // Получаем ник лучшего игрока
+        string nickname = MyDataBase.ExecuteQueryWithAnswer($"SELECT nickname FROM Player WHERE id_player = {idBestPlayer};");
+        Debug.Log($"Лучший игрок {nickname} набрал {scoreboard.Rows[0][2].ToString()} очков.");
+    }
+}
