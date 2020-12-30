0b6bec60eed76e4456ed7cb90d4a82f5b7da241d
MetraSib <70145795+MetraSib@users.noreply.github.com>
2020-12-30 14:06:02 +0300

    Добавленен Unity Analitics.

---
+++ Assets/Scripts/UnityAnaliticsComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class UnityAnaliticsComponent : MonoBehaviour
+{
+    public void OnStartGame(int levelId)
+    {
+        Analytics.CustomEvent("Player Dead", new Dictionary<string, object>
+        {
+            {"Level", levelId },
+        });
+    }
+}
